90f35f1c4d9b2a4aa3c9d2c46908c629550850b4
pankerit <pankerbest@gmail.com>
2023-04-27 14:29:55 +0300

    runtime edge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,10 @@ import { NoSsr } from "@/components/no-ssr";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const config = {
+  runtime: "edge",
+};
+
 export const getServerSideProps = async () => {
   return {
     props: {
